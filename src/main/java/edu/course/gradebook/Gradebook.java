41e4fbac453ed7d9040957ea4e944790983796bf
Rikkim1 <rmann@student.touro.edu>
2026-02-09 22:21:32 -0500

    I tried but I really dont know what I am doing

--- src/main/java/edu/course/gradebook/Gradebook.java
+++ src/main/java/edu/course/gradebook/Gradebook.java
@@ -13,29 +13,83 @@ public class Gradebook {
     }
 
     public boolean addStudent(String name) {
-        throw new UnsupportedOperationException();
+        if (gradesByStudent.containsKey(name)) {
+            return false;
+        }
+        gradesByStudent.put(name, new ArrayList<>());
+        activityLog.push("added student " + name);
+        return true;
+
     }
 
     public boolean addGrade(String name, int grade) {
-        throw new UnsupportedOperationException();
+        if (gradesByStudent.containsKey(name)) {
+            gradesByStudent.get(name).add(grade);
+
+            undoStack.push(new UndoAction() {
+                @Override
+                public void undo(Gradebook gradebook) {
+
+                }
+            });
+
+
+            activityLog.push("Added grade " + grade + " for " + name);
+            return true;
+        }
+        return false;
     }
 
     public boolean removeStudent(String name) {
-        throw new UnsupportedOperationException();
+        if (gradesByStudent.containsKey(name)) {
+            gradesByStudent.get(name).remove(name);
+            undoStack.push(new UndoAction() {
+                @Override
+                public void undo(Gradebook gradebook) {
+                }
+            });
+            activityLog.push("removed student " + name);
+            return true;
+        }
+        return false;
     }
 
     public Optional<Double> averageFor(String name) {
-        throw new UnsupportedOperationException();
+        double grade = 0;
+        if (gradesByStudent.containsKey(name)) {
+            for (String student : gradesByStudent.keySet()) {
+                grade += gradesByStudent.get(student).get((int) grade);
+            }
+
+            grade /= gradesByStudent.size();
+            return Optional.ofNullable(grade);
+        }
+        return Optional.empty();
     }
 
-    public Optional<String> letterGradeFor(String name) {
-        throw new UnsupportedOperationException();
+    public Optional<List<Integer>> letterGradeFor(String name) {
+        if (gradesByStudent.containsKey(name)) {
+            var x = gradesByStudent.get(averageFor(name));
+            switch (x) {
+                case 90, 100 -> System.out.print("A");
+                case 80, 89 -> System.out.print("B");
+                case 70, 79 -> System.out.print("C");
+                case 60, 69 -> System.out.print("D");
+                default -> System.out.print("F");
+
+            }
+            System.out.println(": " + x);
+            return Optional.ofNullable(x);
+        }
+
+
+        return Optional.empty();
     }
 
-    public Optional<Double> classAverage() {
-        throw new UnsupportedOperationException();
-    }
+    public Optional<Double> classAverage()
+    {
 
+    }
     public boolean undo() {
         throw new UnsupportedOperationException();
     }
